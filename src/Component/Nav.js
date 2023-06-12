ada81d2ed383effabfff183a3f48f5bd897af94d
Nizara <67256107+Nishara-Sewminie@users.noreply.github.com>
2023-06-12 22:49:02 +0530

    Add files via upload

---
+++ src/Component/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import './nav.css';
+import {Link} from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="navbar">
+          <Link to="/" className='nav-item'>Homepage</Link>
+          <Link to="/about" className='nav-item'>About</Link>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
